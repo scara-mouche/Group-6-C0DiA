104c76c36af48f6332dcd5e8ccef3b51328ee415
scara-mouche <scaram127@gmail.com>
2026-03-31 13:06:34 +0800

    Delete Codia directory

--- Codia/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset();   // burahin lahat ng session variables
-session_destroy(); // sirain ang session
-header("Location: index.php"); // redirect sa login page
-exit();
-?>
